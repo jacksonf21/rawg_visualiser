ba1c41ca3c05e2480d47fce97dbf68892f2b6fe3
jacksonf21 <jacksonf21@hotmail.com>
2019-10-31 14:02:40 -0400

    add sass stylesheets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import Content from './components/Content.component';
-import './App.css';
+// import './App.css';
 
 function App() {
   const [state, setState] = useState([]);
--- src/components/Content.component.jsx
+++ src/components/Content.component.jsx
@@ -1,48 +1,60 @@
 import React from 'react';
 import Visualiser from './Visualiser.component';
 import ComparitiveVisualiser from './ComparitiveVisualiser.component';
+import '../stylesheets/content.css';
 
 export default function Header({ nextGame, previousGame, games, select, gamesAPIdata }) {
   const topTwenty = games.map(game => {
     return (
       <main>
-        <header>
-          <div>{game.name}</div>
-          <div>{game.rating}</div>
-          <div>{game.ratingsCount}</div>
-        </header>
-        <article>
-          <Visualiser
-            ratings={game.ratings}
-          />
-        </article>
+        <div className='game-header__title'>{game.name}</div>
+        <div className='game-header__rating'>{game.rating}</div>
+        <div className='game-header__count'>{game.ratingsCount}</div>
       </main>
     );
   });
   
+  const visualPiece = games.map(game => {
+    return (
+      <article>
+      <Visualiser
+        ratings={game.ratings}
+      />
+    </article>
+)
+  })
+
   return (
     <div>
-      <div>
+      <header className='game-header'>
         {topTwenty[select]}
+        
         <div onClick={() => nextGame()}>
           Forward
         </div>
+        
         <div onClick={() => previousGame()}>
           Backward
         </div>
-      </div>
+        
+        <nav className='game-header__nav'>
+          <div onClick={() => gamesAPIdata('http://localhost:8000')}>
+            THIS YEAR
+          </div>
+          <div onClick={() => gamesAPIdata('http://localhost:8000/this-month')}>
+            THIS MONTH
+          </div>
+          <div onClick={() => gamesAPIdata('http://localhost:8000/upcoming-month')}>
+            NEXT MONTH
+          </div>
+        </nav>
+
+      </header>
+
+      {visualPiece[select]}
       <ComparitiveVisualiser 
         topTwenty={topTwenty}
       />
-      <div onClick={() => gamesAPIdata('http://localhost:8000')}>
-        THIS YEAR
-      </div>
-      <div onClick={() => gamesAPIdata('http://localhost:8000/this-month')}>
-        THIS MONTH
-      </div>
-      <div onClick={() => gamesAPIdata('http://localhost:8000/upcoming-month')}>
-        NEXT MONTH
-      </div>
     </div>
   );
 };
